0bed2bfaf181f6d4d1038e38b47c8026fc74fed4
campbell88 <shatharcampbell@gmail.com>
2017-04-20 16:57:27 -0400

    first commit with valid prime method

---
+++ Lab16/PrimeMethod.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab16
+{
+    class PrimeMethod
+    {
+
+        static void Main(string[] args)
+
+        {
+            string num1;
+
+            Console.WriteLine("Let's lcate some primes!');
+            
+           Conssole.WriteLine("This application will find you any prime, in order from first prime")
+
+
+
+            //Console.WriteLine("Accept number:");
+            //num1 = Convert.ToInt32(Console.ReadLine());
+            //if (isPrime(num1))
+            //{
+            //    Console.WriteLine("It is prime");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("It is not prime");
+            //}
+        }
+
+        public static bool isPrime(int number)
+        {
+            if (number == 1) return false;
+            if (number == 2) return true;
+
+            var boundary = (int)Math.Floor(Math.Sqrt(number));
+
+            for (int i = 2; i <= boundary; ++i)
+            {
+                if (number % i == 0) return false;
+            }
+
+            return true;
+        }
+
+
+    }
+    }
+
---
+++ Lab16/UnitTest1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Lab16
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            bool Expected = false;
+            Assert.AreEqual(Expected, PrimeMethod.isPrime(1));
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            bool Expected = true;
+            Assert.AreEqual(Expected, PrimeMethod.isPrime(2));
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            bool Expected = true;
+            Assert.AreEqual(Expected, PrimeMethod.isPrime(3));
+        }
+    }
+
+}
